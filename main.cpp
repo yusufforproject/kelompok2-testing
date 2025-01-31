62279766def6f155045e382b5dd2907bcc03ee92
Zekisptn <150105141+Zekisptn@users.noreply.github.com>
2025-01-31 11:35:08 +0700

    update again main (#8)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, kelompok 2" << std::endl;
+    std::cout << "Hello, apa kabar kalian semua?" << std::endl;
     return 0;
 }
