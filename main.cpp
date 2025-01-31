b063f1eaef90d850c2eade37f8f444500ea74828
dchristofer7 <danielmarvel46@gmail.com>
2025-01-31 16:13:54 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, apa kabar kalian semua?" << std::endl;
+    std::cout << "Hello, aku kabar baik?" << std::endl;
     return 0;
 }
